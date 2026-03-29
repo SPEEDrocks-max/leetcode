bfc4077516496d7f656e1197fcd8fdddaa8026e4
Shourya Kumar <shouryaarnav4@gmail.com>
2026-03-29 12:19:47 +0530

    Time: 36 ms (30.41%) | Memory: 59.2 MB (53.96%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -8,7 +8,7 @@ class Solution {
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            // Step 2: skip duplicate i
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int j = i + 1;               
